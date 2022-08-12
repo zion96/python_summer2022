a17d6d35c71c8edb518074643ffd50320c35121b
afprati <aprati@wustl.edu>
2022-08-12 14:14:25 -0500

    Adding template files for API authentication

---
+++ Day6/Lecture/start_spotify.py
@@ -0,0 +1,12 @@
+## MOVE THIS FILE OFF GITHUB REPO BEFORE SYNCING!
+
+# Get a Spotify developer account (or login if you have a Spotify account):
+#    https://developer.spotify.com/dashboard/applications
+# "Create App" and fill in name and description. Return to dashboard.
+# After saving, "Edit Settings" and change redirect url to a local host,
+#   e.g. http://localhost:80, click "Add" and then click "Save"
+# Copy/paste to fill in below:
+
+client_id='YOUR CLIENT ID'
+client_secret='YOUR CLIENT SECRET'
+redirect_url='YOUR REDIRECT URL'
